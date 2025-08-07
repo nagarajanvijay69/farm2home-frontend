dc12088e6e54d524c1f94d81e15c3b6e86621b3f
nagarajanvijay69 <nagarajanvijay69@gmail.com>
2025-08-07 10:40:09 +0530

    save

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -84,11 +84,12 @@ const Products = () => {
               <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 30 30" fill="#6B7280">
                 <path d="M13 3C7.489 3 3 7.489 3 13s4.489 10 10 10a9.95 9.95 0 0 0 6.322-2.264l5.971 5.971a1 1 0 1 0 1.414-1.414l-5.97-5.97A9.95 9.95 0 0 0 23 13c0-5.511-4.489-10-10-10m0 2c4.43 0 8 3.57 8 8s-3.57 8-8 8-8-3.57-8-8 3.57-8 8-8" />
               </svg>
-              <input type="search" placeholder="Search for products" className="w-full h-full outline-none text-gray-500 placeholder-gray-500 text-sm pr-2"
+              <input type="text" placeholder="Search for products" className="w-full h-full outline-none text-gray-500 placeholder-gray-500 text-sm pr-2"
                 onChange={(e) => {
                   setTemp(e.target.value);
                   SearchCheck(e.target.value);
-                  if(e.key == 'Enter' || e.key == 'search') onSearch()
+                  console.log(e.key);
+                  if (e.key === 'Enter' || e.key === 'search') onSearch()
                 }} />
             </div>
             <button type="submit" className="bg-green-500 w-32 h-[46px] rounded-md text-sm text-white" onClick={() => onSearch()}>Search</button>
