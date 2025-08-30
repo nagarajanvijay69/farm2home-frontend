9c02ab332636596706d6961afa7caf862433da43
Nagarajan <nagarajanvijay69@gmail.com>
2025-08-30 21:51:16 +0530

    Update AdminDash.jsx

--- src/admin/AdminDash.jsx
+++ src/admin/AdminDash.jsx
@@ -73,7 +73,7 @@ const AdminDash = () => {
                                                     <span className="truncate max-sm:hidden w-full">{product.name}</span>
                                                 </td>
                                                 <td className="px-4 py-3">{product.category}</td>
-                                                <td className="px-4 py-3 max-sm:hidden">${product.offerPrice}</td>
+                                                <td className="px-4 py-3 max-sm:hidden">₹{product.offerPrice}</td>
                                                 <td className="px-4 py-3">
                                                     <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
                                                         {/* <input type="checkbox" className="sr-only peer" defaultChecked={product.inStock} /> */}
